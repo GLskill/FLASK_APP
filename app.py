582be9eb1bbd6adbc306668432e6148ced9ffa2d
GLskill <Glebluz1488@gmail.com>
2024-09-18 18:08:26 +0200

    creating a base page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Imports
-from flask import Flask
+from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # Route for the home page
 @app.route("/")
 def index():
-    return "Testing 123"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
