9eab02095cc99bc1e115392ad5aa6b6817291e56
GLskill <Glebluz1488@gmail.com>
2024-09-20 12:09:14 +0200

    add html data

--- app.py
+++ app.py
@@ -1,17 +1,50 @@
 # Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 # My App
 app = Flask(__name__)
+Scss(app)
+
+# Database configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+
+# Task model
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+
+    def __repr__(self) -> str:
+        return f"Task {self.id}: {self.content}"
 
 
 # Route for the home page
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    # Add task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"An error occurred while adding the task: {e}"
+
+    # Retrieve all tasks
+    tasks = MyTask.query.order_by(MyTask.created).all()
+    return render_template('index.html', tasks=tasks)
 
 
+# Run the app
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
